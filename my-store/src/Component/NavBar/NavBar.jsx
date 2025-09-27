b4171788d7e8cfa2d06f665de402571ca18c0041
GharbiHaithem <gharbi.haythem1988@gmail.com>
2025-09-27 02:40:45 +0200

    first commit

--- my-store/src/Component/NavBar/NavBar.jsx
+++ my-store/src/Component/NavBar/NavBar.jsx
@@ -15,14 +15,14 @@ const NavBar = ({ setOpenMenu, openMenu, setOpenSearch, cartitem }) => {
           location.pathname === "/checkout" ? "hidden" : ""
         } fixed top-0 left-1/2 -translate-x-1/2 w-full md:w-[80%] z-20`}
       >
-        <div className="w-full h-[80px] px-6 bg-white shadow-sm flex items-center justify-between rounded-b-2xl">
+        <div className="w-full h-[80px] px-6 bg-white  flex items-center justify-between ">
           {/* Menu desktop */}
           <div className="hidden md:flex gap-6 items-center text-gray-600 font-medium">
             <Link
               to="/"
               className={`${
                 location.pathname === "/" ? "text-black underline" : "hover:text-black"
-              } transition text-sm`}
+              } transition text-3xl font-extrabold`}
             >
               Home
             </Link>
@@ -30,7 +30,7 @@ const NavBar = ({ setOpenMenu, openMenu, setOpenSearch, cartitem }) => {
               to="/contact"
               className={`${
                 location.pathname === "/contact" ? "text-black underline" : "hover:text-black"
-              } transition text-sm`}
+              } transition  text-3xl font-extrabold`}
             >
               Contact
             </Link>
@@ -40,12 +40,12 @@ const NavBar = ({ setOpenMenu, openMenu, setOpenSearch, cartitem }) => {
           <div className="md:hidden flex items-center justify-center">
             {!openMenu ? (
               <CiMenuBurger
-                className="w-7 h-7 text-gray-700 cursor-pointer hover:text-black transition"
+                className="w-10 h-10 text-gray-700 cursor-pointer hover:text-black transition"
                 onClick={() => setOpenMenu(true)}
               />
             ) : (
               <TfiClose
-                className="w-7 h-7 text-gray-700 cursor-pointer hover:text-black transition"
+                className="w-10 h-10 text-gray-700 cursor-pointer hover:text-black transition"
                 onClick={() => setOpenMenu(false)}
               />
             )}
@@ -55,24 +55,24 @@ const NavBar = ({ setOpenMenu, openMenu, setOpenSearch, cartitem }) => {
           <img
             src={x}
             alt="Logo"
-            className="h-[65px] w-[65px] object-contain cursor-pointer"
+            className="h-[85px] w-[85px] object-contain cursor-pointer"
             onClick={() => navigate("/")}
           />
 
           {/* Icons */}
           <div className="flex gap-6 items-center text-gray-700">
             <CiSearch
-              className="w-8 h-8 cursor-pointer hover:text-black transition"
+              className="w-10 h-10 cursor-pointer hover:text-black transition"
               onClick={() => setOpenSearch(true)}
             />
             <CiUser
-              className="w-8 h-8 cursor-pointer hover:text-black transition"
+              className="w-10 h-10 cursor-pointer hover:text-black transition"
               onClick={() => navigate("/login")}
             />
             <div className="relative">
               <CiShop
                 onClick={() => navigate("/cart")}
-                className="w-8 h-8 cursor-pointer hover:text-black transition"
+                className="w-10 h-10 cursor-pointer hover:text-black transition"
               />
               <span className="absolute -bottom-2 -right-2 bg-black text-white text-xs w-5 h-5 flex items-center justify-center rounded-full border-2 border-white">
                 {cartitem ? cartitem : 0}
